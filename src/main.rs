dd4254361893657e54d6e5d4f76bd237aab297f5
Yann Simon <yann.simon@commercetools.de>
2020-08-12 08:09:20 +0200

    minimal http server with health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind on port 8080");
+    run(listener)?.await
 }
