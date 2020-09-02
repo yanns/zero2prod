b5807f81db5379e872c0bdbb5ee993128cb055ed
Yann Simon <yann.simon@commercetools.de>
2020-09-02 16:20:54 +0200

    add subscriptions

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,33 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct SubscriptionDraft {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(
+    draft: web::Form<SubscriptionDraft>,
+    connection: web::Data<PgPool>,
+) -> Result<HttpResponse, HttpResponse> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        draft.email,
+        draft.name,
+        Utc::now()
+    )
+    .execute(connection.get_ref())
+    .await
+    .map_err(|e| {
+        eprintln!("Failed to execute query: {}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
+    Ok(HttpResponse::Ok().finish())
+}
