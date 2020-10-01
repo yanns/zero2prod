acad86a1e4f1f3673555814916b9ee0abacdbeaf
Yann Simon <yann.simon.fr@gmail.com>
2020-10-01 17:32:59 +0200

    fix recommanded by clippy

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -17,7 +17,7 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name.into(), std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
