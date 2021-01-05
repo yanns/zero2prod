003d3dd790ccb9f76eb3d42d66e91213539ce967
Yann Simon <yann.simon@commercetools.de>
2021-01-05 17:19:06 +0100

    validation in domain

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use unicode_segmentation::UnicodeSegmentation;
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct SubscribeRequest {
@@ -23,33 +23,28 @@ pub async fn subscribe(
     payload: web::Form<SubscribeRequest>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    if !is_valid_name(&payload.name) {
-        return Err(HttpResponse::BadRequest().finish());
-    }
     if payload.email.trim().is_empty() {
         return Err(HttpResponse::BadRequest().finish());
     }
-    insert_subscriber(&pool, &payload)
+    let name = SubscriberName::parse(payload.0.name)
+        .map_err(|_| HttpResponse::BadRequest().finish())?;
+    let new_subscriber = NewSubscriber {
+        email: payload.0.email,
+        name,
+    };
+    insert_subscriber(&pool, &new_subscriber)
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
     Ok(HttpResponse::Ok().finish())
 }
 
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
-}
-
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(payload, pool)
+    skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(
     pool: &PgPool,
-    payload: &SubscribeRequest,
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -57,8 +52,8 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
             "#,
         Uuid::new_v4(),
-        payload.email,
-        payload.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
