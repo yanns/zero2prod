40bc8cb37f650a3b648c021c81213789177f7451
Yann Simon <yann.simon@commercetools.de>
2021-01-05 18:03:15 +0100

    validate email

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -59,7 +59,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
     let test_cases = vec![
         ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
         ("name=Ursula&email=", "empty email"),
-        // ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
     ];
 
     for (body, description) in test_cases {
