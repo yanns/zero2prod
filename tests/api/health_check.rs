6df0a5d10904fd7a74960387b757f3e9acc6650a
Yann Simon <yann.simon.fr@gmail.com>
2021-03-22 13:47:39 +0100

    new test structure

---
+++ tests/api/health_check.rs
@@ -0,0 +1,19 @@
+use crate::helpers::spawn_app;
+
+#[actix_rt::test]
+async fn health_check_works() {
+    // Given
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // When
+    let response = client
+        .get(&format!("{}/health_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Then
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
